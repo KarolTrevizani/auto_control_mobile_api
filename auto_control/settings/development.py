b3d694b37c3d4dcb8004e245638ed937870ae276
Emanuel Vidal <emanuelvidalrm@gmail.com>
2024-09-11 18:15:55 -0300

    :whale: dockerfile and minor settings changes

--- auto_control/settings/development.py
+++ auto_control/settings/development.py
@@ -13,18 +13,19 @@ INSTALLED_APPS += ['debug_toolbar', ]
 
 # Development-specific middleware
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+ALLOWED_HOSTS = ['10.50.100.154', 'localhost', '127.0.0.1', '*']
 
 # Emanuel Test DB
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mysql',
-        'PORT': '3306',
-        'PASSWORD': '12345678',
-        'USER': 'root',
-        'HOST': '127.0.0.1',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'autocontrol_database',
+#         'PORT': '3306',
+#         'PASSWORD': '12345678',
+#         'USER': 'root',
+#         'HOST': 'localhost',
+#     }
+# }
 
 # Karol Test DB
 DATABASES = {
